7f4ccf2fbd6f907bbffcb84c8567fa937dd4c7b7
Francis Mwangi <fmwangi@microsoft.com>
2022-11-30 10:46:56 +0300

    initial commit

---
+++ src/test/kotlin/com/demo/sprintbootdemoproject/SprintBootDemoProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.sprintbootdemoproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SprintBootDemoProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
